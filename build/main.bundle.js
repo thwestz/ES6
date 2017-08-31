469a9a734a0c982908fe5f2c983a8cab684a8a1e
THWest <thwest001@hotmail.com>
2017-08-31 14:47:42 +0700

    add virstual keyboard

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -1,1 +1,38 @@
-"use strict";
+var toUpper = false;
+var isShift = false;
+function main(input) {
+    let output = document.getElementById("demo").textContent;
+    if (toUpper) {
+        output = output + String(input).toLocaleUpperCase();
+        document.getElementById("demo").innerHTML = output;
+    } else if (isShift) {
+        output = output + String(input).toLocaleUpperCase();
+        document.getElementById("demo").innerHTML = output;
+        isShift = false;
+        document.getElementById("shift").style.backgroundColor = "transparent";
+    } else {
+        output = output + input;
+        document.getElementById("demo").innerHTML = output;
+    }
+}
+function backspace() {
+    let output = document.getElementById("demo").textContent;
+    output = output.substring(0, output.length - 1)
+    document.getElementById("demo").innerHTML = output;
+}
+function upperCase() {
+    toUpper = !toUpper
+    return toUpper;
+}
+function shift() {
+    isShift = !isShift
+    if (isShift) {
+        document.getElementById("shift").style.backgroundColor = "#ffff99";
+    }else{
+        document.getElementById("shift").style.backgroundColor = "transparent";
+    }
+    return isShift;
+}
+function reset() {
+    document.getElementById("demo").innerHTML = "";
+}
